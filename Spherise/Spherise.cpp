63471d43f068d93f8709ed364819481a23e3683a
AnDyXX <a.kurzeja@gmail.com>
2015-03-04 21:02:27 +0100

    made plugin finally to work - the problem was wrongly work of mult/premult functions

--- Spherise/Spherise.cpp
+++ Spherise/Spherise.cpp
@@ -14,8 +14,6 @@ Copyright (C) 2015 AnDyX
 #include <windows.h>
 #endif
 
-#include "ofxsTransformInteract.h"
-
 #include "ofxsProcessing.H"
 #include "ofxsMaskMix.h"
 #include "ofxsMacros.h"
@@ -35,28 +33,108 @@ Copyright (C) 2015 AnDyX
 #define kSupportsMultipleClipDepths false
 #define kRenderThreadSafety eRenderFullySafe
 
-#define kParamTransformTranslate "translate"
-#define kParamTransformTranslateLabel "Translate"
+#define kParamCenterPoint "centerPoint"
+#define kParamCenterPointLabel "Center Point"
 
 
 using namespace OFX;
 
+//base class of Spherise plugin
+class SpheriseProcessorBase : public OFX::ImageProcessor
+{
+protected:
+	const OFX::Image *_srcImg;
+public:
+	SpheriseProcessorBase(OFX::ImageEffect &instance)
+		: OFX::ImageProcessor(instance)
+		, _srcImg(0)
+	{
+		}
+
+	void setSrcImg(const OFX::Image *v) { _srcImg = v; }
+};
+
+
+// Spherise plugin processor
+
+template <class PIX, int nComponents, int maxValue>
+class SpheriseProcessor : public SpheriseProcessorBase
+{
+public:
+	SpheriseProcessor(OFX::ImageEffect &instance)
+		: SpheriseProcessorBase(instance)
+	{
+	}
+
+private:
+
+	void multiThreadProcessImages(OfxRectI procWindow)
+	{
+		if (nComponents == 1) {
+			return process<false, false, false, true >(procWindow);
+		}
+		else if (nComponents == 3) {
+			return process<true, true, true, false>(procWindow);
+		}
+		else if (nComponents == 4) {
+			return process<true, true, true, true >(procWindow);
+		}
+	}
+
+	template<bool processR, bool processG, bool processB, bool processA>
+	void process(const OfxRectI& procWindow)
+	{
+		assert(nComponents == 1 || nComponents == 3 || nComponents == 4);
+		assert(_dstImg);
+
+		for (int y = procWindow.y1; y < procWindow.y2; y++) {
+			if (_effect.abort()) {
+				break;
+			}
+
+			PIX *dstPix = (PIX *)_dstImg->getPixelAddress(procWindow.x1, y);
+
+			//raw values from pixel in 0-1
+			float unpPix[4];
+
+			for (int x = procWindow.x1; x < procWindow.x2; x++) {
+				//get src pixel
+				const PIX *srcPix = (const PIX *)(_srcImg ? _srcImg->getPixelAddress(x, y) : 0);
+				//normalise values to 0-1
+				ofxsUnPremult<PIX, nComponents, maxValue>(srcPix, unpPix, true, 0);
+
+				//unpPix[0] = 0.0f;
+				//unpPix[2] = 0.1;
+
+				ofxsPremultMaskMixPix<PIX, nComponents, maxValue, false>(unpPix, false, 0, x, y, srcPix, false, 0, 1.0f, false, dstPix);
+				//ofxsJustPremult2<PIX, nComponents, maxValue, false>(unpPix, dstPix);
+				//ofxsJustPremult<PIX, nComponents, maxValue, false>(unpPix, dstPix);
+				// increment the dst pixel
+				dstPix += nComponents;
+			}
+		}
+	}
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 /** @brief The plugin that does our work */
-class AddSpherisePlugin : public OFX::ImageEffect
+class SpherisePlugin : public OFX::ImageEffect
 {
 public:
 	/** @brief ctor */
-	AddSpherisePlugin(OfxImageEffectHandle handle)
+	SpherisePlugin(OfxImageEffectHandle handle)
 		: ImageEffect(handle)
 		, _dstClip(0)
 		, _srcClip(0)
+		, _centerPoint(0)
 	{
 		_dstClip = fetchClip(kOfxImageEffectOutputClipName);
 		assert(_dstClip && (_dstClip->getPixelComponents() == ePixelComponentAlpha || _dstClip->getPixelComponents() == ePixelComponentRGB || _dstClip->getPixelComponents() == ePixelComponentRGBA));
 		_srcClip = fetchClip(kOfxImageEffectSimpleSourceClipName);
 		assert(_srcClip && (_srcClip->getPixelComponents() == ePixelComponentAlpha || _srcClip->getPixelComponents() == ePixelComponentRGB || _srcClip->getPixelComponents() == ePixelComponentRGBA));
+
+		// NON-GENERIC
+		_centerPoint = fetchDouble2DParam(kParamCenterPoint);
 	}
 private:
 	/* Override the render */
@@ -66,36 +144,166 @@ private:
 
 	virtual void changedParam(const OFX::InstanceChangedArgs &args, const std::string &paramName) OVERRIDE FINAL;
 
+	/* set up and run a processor */
+	void setupAndProcess(SpheriseProcessorBase & processor, const OFX::RenderArguments &args);
+
 private:
 	// do not need to delete these, the ImageEffect is managing them for us
 	OFX::Clip *_dstClip;
 	OFX::Clip *_srcClip;
+
+	// NON-GENERIC
+	OFX::Double2DParam* _centerPoint;
 };
 
 
+void
+SpherisePlugin::setupAndProcess(SpheriseProcessorBase & processor, const OFX::RenderArguments &args){
+	std::auto_ptr<OFX::Image> dst(_dstClip->fetchImage(args.time));
+	if (!dst.get()) {
+		OFX::throwSuiteStatusException(kOfxStatFailed);
+	}
+	OFX::BitDepthEnum         dstBitDepth = dst->getPixelDepth();
+	OFX::PixelComponentEnum   dstComponents = dst->getPixelComponents();
+	if (dstBitDepth != _dstClip->getPixelDepth() ||
+		dstComponents != _dstClip->getPixelComponents()) {
+		setPersistentMessage(OFX::Message::eMessageError, "", "OFX Host gave image with wrong depth or components");
+		OFX::throwSuiteStatusException(kOfxStatFailed);
+	}
+	if (dst->getRenderScale().x != args.renderScale.x ||
+		dst->getRenderScale().y != args.renderScale.y ||
+		(dst->getField() != OFX::eFieldNone /* for DaVinci Resolve */ && dst->getField() != args.fieldToRender)) {
+		setPersistentMessage(OFX::Message::eMessageError, "", "OFX Host gave image with wrong scale or field properties");
+		OFX::throwSuiteStatusException(kOfxStatFailed);
+	}
+	std::auto_ptr<const OFX::Image> src((_srcClip && _srcClip->isConnected()) ?
+		_srcClip->fetchImage(args.time) : 0);
+	if (src.get()) {
+		if (src->getRenderScale().x != args.renderScale.x ||
+			src->getRenderScale().y != args.renderScale.y ||
+			(src->getField() != OFX::eFieldNone /* for DaVinci Resolve */ && src->getField() != args.fieldToRender)) {
+			setPersistentMessage(OFX::Message::eMessageError, "", "OFX Host gave image with wrong scale or field properties");
+			OFX::throwSuiteStatusException(kOfxStatFailed);
+		}
+		OFX::BitDepthEnum    srcBitDepth = src->getPixelDepth();
+		OFX::PixelComponentEnum srcComponents = src->getPixelComponents();
+		if (srcBitDepth != dstBitDepth || srcComponents != dstComponents) {
+			OFX::throwSuiteStatusException(kOfxStatErrImageFormat);
+		}
+	}
+
+	processor.setDstImg(dst.get());
+	processor.setSrcImg(src.get());
+	processor.setRenderWindow(args.renderWindow);
+
+	processor.process();
+
+}
+
 // the overridden render function
 void
-AddSpherisePlugin::render(const OFX::RenderArguments &args)
+SpherisePlugin::render(const OFX::RenderArguments &args)
 {
+	// instantiate the render code based on the pixel depth of the dst clip
+	OFX::BitDepthEnum       dstBitDepth = _dstClip->getPixelDepth();
+	OFX::PixelComponentEnum dstComponents = _dstClip->getPixelComponents();
+
+	assert(kSupportsMultipleClipPARs || !_srcClip || _srcClip->getPixelAspectRatio() == _dstClip->getPixelAspectRatio());
+	assert(kSupportsMultipleClipDepths || !_srcClip || _srcClip->getPixelDepth() == _dstClip->getPixelDepth());
+	assert(dstComponents == OFX::ePixelComponentAlpha || dstComponents == OFX::ePixelComponentRGB || dstComponents == OFX::ePixelComponentRGBA);
+
+	if (dstComponents == OFX::ePixelComponentAlpha) {
+		switch (dstBitDepth) {
+		case OFX::eBitDepthUByte: {
+									  SpheriseProcessor<unsigned char, 1, 255> fred(*this);
+									  setupAndProcess(fred, args);
+									  break;
+		}
+		case OFX::eBitDepthUShort: {
+									   SpheriseProcessor<unsigned short, 1, 65535> fred(*this);
+									   setupAndProcess(fred, args);
+									   break;
+		}
+		case OFX::eBitDepthFloat: {
+									  SpheriseProcessor<float, 1, 1> fred(*this);
+									  setupAndProcess(fred, args);
+									  break;
+		}
+		default:
+			OFX::throwSuiteStatusException(kOfxStatErrUnsupported);
+		}
+	}
+	else if (dstComponents == OFX::ePixelComponentRGBA) {
+		switch (dstBitDepth) {
+		case OFX::eBitDepthUByte: {
+									  SpheriseProcessor<unsigned char, 4, 255> fred(*this);
+									  setupAndProcess(fred, args);
+									  break;
+		}
+		case OFX::eBitDepthUShort: {
+									   SpheriseProcessor<unsigned short, 4, 65535> fred(*this);
+									   setupAndProcess(fred, args);
+									   break;
+		}
+		case OFX::eBitDepthFloat: {
+									  SpheriseProcessor<float, 4, 1> fred(*this);
+									  setupAndProcess(fred, args);
+									  break;
+		}
+		default:
+			OFX::throwSuiteStatusException(kOfxStatErrUnsupported);
+		}
+	}
+	else {
+		assert(dstComponents == OFX::ePixelComponentRGB);
+		switch (dstBitDepth) {
+		case OFX::eBitDepthUByte: {
+									  SpheriseProcessor<unsigned char, 3, 255> fred(*this);
+									  setupAndProcess(fred, args);
+									  break;
+		}
+		case OFX::eBitDepthUShort: {
+									   SpheriseProcessor<unsigned short, 3, 65535> fred(*this);
+									   setupAndProcess(fred, args);
+									   break;
+		}
+		case OFX::eBitDepthFloat: {
+									  SpheriseProcessor<float, 3, 1> fred(*this);
+									  setupAndProcess(fred, args);
+									  break;
+		}
+		default:
+			OFX::throwSuiteStatusException(kOfxStatErrUnsupported);
+		}
+	}
 }
 
 bool
-AddSpherisePlugin::isIdentity(const IsIdentityArguments &/*args*/, Clip * &identityClip, double &/*identityTime*/)
+SpherisePlugin::isIdentity(const IsIdentityArguments &/*args*/, Clip * &identityClip, double & identityTime)
 {
-	identityClip = _srcClip;
-	return true;
+	// NON-GENERIC
+
+	OfxPointD centerPoint;
+
+	_centerPoint->getValueAtTime(identityTime, centerPoint.x, centerPoint.y);
+
+	if (centerPoint.x == 0. && centerPoint.y == 0.) {
+		return true;
+	}
+
+	return false;
 }
 
 void
-AddSpherisePlugin::changedParam(const OFX::InstanceChangedArgs &args, const std::string &paramName)
+SpherisePlugin::changedParam(const OFX::InstanceChangedArgs &args, const std::string &paramName)
 {
 
 }
 
 //factory
-mDeclarePluginFactory(AddSpheriseFactory, {}, {});
+mDeclarePluginFactory(SpheriseFactory, {}, {});
 
-void AddSpheriseFactory::describe(OFX::ImageEffectDescriptor &desc)
+void SpheriseFactory::describe(OFX::ImageEffectDescriptor &desc)
 {
 	// basic labels
 	desc.setLabel(kPluginName);
@@ -122,52 +330,46 @@ void AddSpheriseFactory::describe(OFX::ImageEffectDescriptor &desc)
 
 }
 
-void AddSpheriseFactory::describeInContext(OFX::ImageEffectDescriptor &desc, OFX::ContextEnum context)
+void SpheriseFactory::describeInContext(OFX::ImageEffectDescriptor &desc, OFX::ContextEnum context)
 {
 	// Source clip only in the filter context
 	// create the mandated source clip
 	ClipDescriptor *srcClip = desc.defineClip(kOfxImageEffectSimpleSourceClipName);
-	srcClip->addSupportedComponent(ePixelComponentNone);
 	srcClip->addSupportedComponent(ePixelComponentRGBA);
 	srcClip->addSupportedComponent(ePixelComponentRGB);
-	srcClip->addSupportedComponent(ePixelComponentAlpha);
-	srcClip->addSupportedComponent(ePixelComponentCustom);
 	srcClip->setTemporalClipAccess(false);
 	srcClip->setSupportsTiles(kSupportsTiles);
 	srcClip->setIsMask(false);
 
 	// create the mandated output clip
 	ClipDescriptor *dstClip = desc.defineClip(kOfxImageEffectOutputClipName);
-	dstClip->addSupportedComponent(ePixelComponentNone);
 	dstClip->addSupportedComponent(ePixelComponentRGBA);
 	dstClip->addSupportedComponent(ePixelComponentRGB);
-	dstClip->addSupportedComponent(ePixelComponentAlpha);
-	dstClip->addSupportedComponent(ePixelComponentCustom);
 	dstClip->setSupportsTiles(kSupportsTiles);
 
 	// make some pages and to things in
 	PageParamDescriptor *page = desc.definePageParam("Controls");
 	// translate
 	{
-		Double2DParamDescriptor* param = desc.defineDouble2DParam(kParamTransformTranslate);
-		param->setLabel(kParamTransformTranslateLabel);
+		Double2DParamDescriptor* param = desc.defineDouble2DParam(kParamCenterPoint);
+		param->setLabel(kParamCenterPointLabel);
 		param->setDoubleType(eDoubleTypeXYAbsolute);
 		param->setDefault(0, 0);
-		param->setIncrement(10.);
+		param->setIncrement(0.1);
 		if (page) {
 			page->addChild(*param);
 		}
 	}
 }
 
-OFX::ImageEffect* AddSpheriseFactory::createInstance(OfxImageEffectHandle handle, OFX::ContextEnum /*context*/)
+OFX::ImageEffect* SpheriseFactory::createInstance(OfxImageEffectHandle handle, OFX::ContextEnum /*context*/)
 {
-	return new AddSpherisePlugin(handle);
+	return new SpherisePlugin(handle);
 }
 
 //register plugin
 void getSpherisePluginID(OFX::PluginFactoryArray &ids)
 {
-	static AddSpheriseFactory p(kPluginIdentifier, kPluginVersionMajor, kPluginVersionMinor);
+	static SpheriseFactory p(kPluginIdentifier, kPluginVersionMajor, kPluginVersionMinor);
 	ids.push_back(&p);
 }
--- Spherise/Spherise.h
+++ Spherise/Spherise.h
@@ -9,6 +9,32 @@ Copyright (C) 2015 AnDyX
 #define Misc_Spherise_h
 
 #include "ofxsImageEffect.h"
+// unpPix is normalized between [0,1]
+template <class PIX, int nComponents, int maxValue, bool masked>
+void
+ofxsJustPremult2(const float unpPix[4], PIX *dstPix) //!< destination pixel
+{
+	ofxsPremultMaskMixPix<PIX, nComponents, maxValue, false>(unpPix, false, 0, 0, 0, dstPix, false, 0, 1.0f, false, dstPix);
+}
+
+// unpPix is normalized between [0,1]
+template <class PIX, int nComponents, int maxValue, bool masked>
+void
+ofxsJustPremult(const float unpPix[4], PIX *dstPix) //!< destination pixel
+{
+	float tmpPix[nComponents];
+
+	//ofxsPremult<PIX, nComponents, maxValue>(unpPix, tmpPix, false , 0);
+	for (int c = 0; c < nComponents; ++c) {
+		tmpPix[c] = unpPix[c] * maxValue;
+	}
+	
+	// no mask, no mix
+	for (int c = 0; c < nComponents; ++c) {
+		dstPix[c] = PIX(ofxsClampIfInt<maxValue>(tmpPix[c], 0, maxValue));
+	}
+}
+
 
 void getSpherisePluginID(OFX::PluginFactoryArray &ids);
 
