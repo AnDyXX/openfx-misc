9f40306a4b7520aa561f2c3a9f44e1a271cc8abc
AnDyXX <a.kurzeja@gmail.com>
2015-04-05 13:32:18 +0200

    Merged 2015-04-05

---
+++ RadialBlur/RadialBlur.h
@@ -0,0 +1,15 @@
+/*
+AX Radial blur plugin.
+
+Copyright (C) 2015 AnDyX
+
+*/
+
+#ifndef Misc_RadialBlur_h
+#define Misc_RadialBlur_h
+
+#include "ofxsImageEffect.h"
+
+void getRadialBlurID(OFX::PluginFactoryArray &ids);
+
+#endif // Misc_RadialBlur_h
